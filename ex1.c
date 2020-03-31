8c29596442efcb84586cc035707d7eea640f243b
Daniel Alves <dalves@ucsc.edu>
2020-03-30 18:40:20 -0700

    Add printing of file.

--- ex1.c
+++ ex1.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
-  int i;
+  int i, fd, n;
+  char buf[101];
   printf("hello world\n");
   for(i = 1; i < argc; i++) {
     printf("%s\n", argv[i]);
   }
+
+  memset(buf, 0, 101);
+  fd = open(argv[1], O_RDONLY);
+  n = read(fd, buf, 100);
+  printf("%s\n", buf);
+  printf("%d\n", n);
 }
